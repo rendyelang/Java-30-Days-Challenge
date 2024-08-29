4853467de321fe2a738cea44dece5f2ef27e8905
rendyelang <lsmnrndy123@gmail.com>
2024-08-29 20:55:15 +0700

    updated day 10 solution

--- Day 10 - Binary Numbers/Solution.java
+++ Day 10 - Binary Numbers/Solution.java
@@ -2,19 +2,28 @@
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Collections;
 
 public class Solution {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Welcome to decimal to binary converter!");
+        System.out.println("Welcome to number converter!");
         System.out.println("Enter a decimal number: ");
         int decimalValue = scan.nextInt();
 
+        // BINARY RESULT 
         ArrayList<Integer> resultInBinary = decimalToBinary(decimalValue);
+        
+        // DECIMAL RESULT
+        int decimalResult = binaryToDecimal(resultInBinary);
 
-        int consecutiveOneCount = findConsecutiveOne(resultInBinary);
+        // int consecutiveOneCount = findConsecutiveOne(resultInBinary);
 
-        System.out.println(consecutiveOneCount);
+        // System.out.println(consecutiveOneCount);
+
+        System.out.println("Binary value of " + decimalValue + " is: " + resultInBinary);
+        
+        System.out.println("Decimal value of " + resultInBinary + " is: " + decimalResult);
 
         scan.close();
     }
@@ -28,7 +37,20 @@ public class Solution {
             binaryList.add(remainder);
         }
 
+        Collections.reverse(binaryList);
+
         return binaryList;
+    }
+
+    protected static int binaryToDecimal(ArrayList<Integer> binaryValue) {
+        int decimalResult = 0;
+        int power = binaryValue.size()-1;
+        for (int value : binaryValue) {
+            decimalResult += value * Math.pow(2, power);
+            power--;
+        }
+
+        return decimalResult;
     }
 
     protected static int findConsecutiveOne(ArrayList<Integer> binaryList) {
